eea68fc713345d1160ce47db6f96b47304f4233f
Matt Ahn <matthew.hyungjun.ahn@gmail.com>
2020-11-12 20:56:01 -0800

    Example binary that links all projects.

---
+++ example/example.cpp
@@ -0,0 +1,16 @@
+
+// #include <gtest/gtest.h>
+#include <iostream>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+int main(int argc, char *argv[])
+{
+
+	std::cout << "Hello, world!" << std::endl;
+
+    cv::namedWindow("test");
+
+	return 0;
+}
